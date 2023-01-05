1d58f7f6a7674ac834ebe27169f434f3e21d3fa1
Caroline Moran <carol.moran@agenciam3.com>
2023-01-05 15:08:27 -0400

    feat: cria estrutura react do projeto

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { FaleConosco } from "./pages/FaleConosco";
+import "./global.css";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(<FaleConosco />);

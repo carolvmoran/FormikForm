f5cea0d3da6b4a2dc07c6123c635c716cbf846d5
Caroline Moran <carol.moran@agenciam3.com>
2023-01-05 15:10:50 -0400

    feat(FaleConosco): cria breadcrumb e titulo

---
+++ src/components/TitleAndBread/Title.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./TitleAndBread.module.css";
+
+const Titulo = () => {
+  return <h1 className={styles["h1"]}>Institucional</h1>;
+};
+
+export { Titulo };
